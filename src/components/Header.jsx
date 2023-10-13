b3cf0ce99fc8fc54c1252d9858dcd0833586fa11
Luis Gomez <luis.gomez@bigcommerce.com>
2023-10-12 18:30:30 -0600

    font size update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ function Header({ pokemonName, setPokemonName }) {
 
   return (
     <div className="bg-red-400 w-screen rounded-br-xl rounded-bl-xl p-5 flex-col text-center sticky top-0 ">
-      <div className="font-press text-white tracking-widest">
+      <div className="font-press text-white tracking-widest text-sm">
         Which Pókemon are you looking for? :)
       </div>
       <div className="mt-2 flex justify-center">
         <input
-          className="font-press text-lg text-center bg-gray-100 appearance-none border-2 border-gray-200 rounded-full w-80 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-500"
+          className="font-press text-sm text-center bg-gray-100 appearance-none border-2 border-gray-200 rounded-full w-80 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-500"
           type="text"
           placeholder="name goes here"
           value={pokemonName}
